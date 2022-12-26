43402563149381dd7f67aeb8c48a4679bb1fe5e5
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-12-26 14:18:40 +0000

    Update 3 files

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,14 @@
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
-import { getVisibleContacts } from '../../helpers/visibleContacts';
+import { selectVisibleContacts} from 'redux/selectors';
 import { Contact } from './Contact';
 import { ContactListGroup, СontactСard } from './ContactList.styled';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-  const visibleContacts = getVisibleContacts(contacts, filter);
+  const contacts = useSelector(selectVisibleContacts);
 
   return (
     <ContactListGroup>
-      {visibleContacts.map(({ id, name, phone }) => (
+      {contacts.map(({ id, name, phone }) => (
         <СontactСard key={id}>
           <Contact name={name} number={phone} id={id} />
         </СontactСard>
@@ -19,4 +16,3 @@ export const ContactList = () => {
     </ContactListGroup>
   );
 };
-
--- src/helpers/visibleContacts.js
+++
@@ -1,4 +0,0 @@
-export const getVisibleContacts = (contacts, filter) =>
-  contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
